748dcf3311de2351c867716fc03d2d5660546fea
Elianamos <elianamos29@gmail.com>
2022-12-14 22:43:11 +0300

    Some functionalities added

--- app.js
+++ app.js
@@ -1,32 +1,67 @@
 const calculator = document.querySelector('.calculator')
 const keys = calculator.querySelector('.calculator_keys')
+const display = document.querySelector('.calculator_display')
 
 keys.addEventListener('click', e => {
     if(e.target.matches('button')){
          const key = e.target
          const action = key.dataset.action
+         const keyContent = key.textContent
+         const displayContent = display.textContent
+         const previousKeyType = calculator.dataset.previousKeyType
 
-         if(!action){
-            alert("Number key!")
+         if (!action) {
+            if (displayContent === '0' || previousKeyType === 'operator') {
+                display.textContent = keyContent
+            } else {
+                display.textContent = displayContent + keyContent
+            }
          }
 
-        else if(action === 'add' ||
-            action === 'subtract' ||
-            action === 'multiply' ||
-            action === 'divide'){
-                alert('Operator!')
-        }
+         if (action === 'decimal') {
+            display.textContent = displayContent + keyContent
+         }
 
-        else if(action === 'decimal'){
-            alert('Decimal key!')
-        }
+         if (action === 'add' ||
+             action === 'subtract' ||
+             action === 'multiply' ||
+             action === 'divide') {
+                key.classList.add('is-depressed')
+                calculator.dataset.previousKeyType = 'operator'
+                calculator.dataset.firstValue = displayContent
+                calculator.dataset.operator = action
 
-        else if(action === 'clear'){
-            alert('Clear key!')
-        }
+             }
+         
+         if (action === 'calculate') {
+            const firstValue = calculator.dataset.firstValue
+            const operator = calculator.dataset.operator
+            const secondValue = displayContent
+            
+            let result = calculate(firstValue, operator, secondValue)
+            display.textContent = result
+         }
 
-        else if(action === 'calculate'){
-            alert('Equal key!')
-        }
+         Array.from(key.parentNode.children).forEach(k => k.classList.remove('is-depressed'))
     }
 })
+
+const calculate = (n1, op, n2) => {
+    let result = ''
+
+    if (op === 'add') {
+        result = n1 + n2
+    }
+
+    else if (op === 'subtract') {
+        result = n1 - n2
+    }
+
+    else if (op === 'multiply') {
+        result = n1 * n2
+    } else {
+        result = n1 / n2
+    }
+
+    return result
+}
